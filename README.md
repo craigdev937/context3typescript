cad1f24531fd69b029c89d76560d8cd5f25036cf
Craig <indigomx9@gmail.com>
2022-06-22 14:09:20 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Shopping Cart
+
+Initial setup of the project.  Created with React, TypeScript, Vite, React Router, and ContextAPI.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quam fugiat vel cumque culpa, alias officia, error labore dolorem vitae ullam! Quis dolores consequatur a adipisci odio aspernatur voluptate veniam?</p>
+        </React.Fragment>
+    );
+};
+
+
