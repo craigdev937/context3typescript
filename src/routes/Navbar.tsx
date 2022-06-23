c2a84ce9a950ba9811399dc74d0e2b70a2a8cdc9
Craig <indigomx9@gmail.com>
2022-06-22 21:45:40 -0400

    Created the Navbar.

--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -1,16 +1,48 @@
 import React from "react";
+import cart2 from "../img/cart2.svg"
 import { NavLink } from "react-router-dom";
-import { Container, Nav, Navbar as NavbarBS } from "react-bootstrap";
+import { Button, Container, Nav, Navbar as NavbarBS } from "react-bootstrap";
 
 export const Navbar = (): JSX.Element => (
     <React.Fragment>
-        <NavbarBS className="bg-white shadow-sm mb-3">
-            <Container>
+    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
+        <Container>
+            <Nav className="me-auto">
                 <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
                 <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
                 <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
-            </Container>
-        </NavbarBS>
+                <Button 
+                    style={{ 
+                        width: "3rem", 
+                        height: "3rem", 
+                        position: "relative"
+                    }}
+                    variant="outline-primary"
+                    className="rounded-circle"
+                    >
+                        <img 
+                            width="30rem" height="30rem"
+                            src={cart2} alt="cart" 
+                        />
+                        <aside className="rounded-circle 
+                            bg-danger d-flex 
+                            justify-content-center 
+                            align-items-center"
+                            style={{
+                                color: "white", 
+                                width: "1.5rem", 
+                                height: "1.5rem",
+                                position: "absolute",
+                                bottom: "0", 
+                                right: "0", 
+                                transform: "translate(25%, 25%)",
+                            }}
+                            >
+                        </aside>
+                </Button>
+            </Nav>
+        </Container>
+    </NavbarBS>
     </React.Fragment>
 );
 
