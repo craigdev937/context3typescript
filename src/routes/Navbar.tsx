f3ddcbb0dbf284dbde05343dcb80c683052366cd
Craig <indigomx9@gmail.com>
2022-06-24 16:29:44 -0400

    Updated the Navbar.

--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -15,6 +15,8 @@ export const Navbar = (): JSX.Element => {
                     <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
                     <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
                     <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+                </Nav>
+                {cartQty > 0 && (
                     <Button 
                         style={{ 
                             width: "3rem", 
@@ -25,26 +27,26 @@ export const Navbar = (): JSX.Element => {
                         className="rounded-circle"
                         onClick={openCart}
                         >
-                            <img 
-                                width="30rem" height="30rem"
-                                src={cart2} alt="cart" 
-                            />
-                            <aside className="rounded-circle 
-                                bg-danger d-flex 
-                                justify-content-center 
-                                align-items-center"
-                                style={{
-                                    color: "white", 
-                                    width: "1.5rem", 
-                                    height: "1.5rem",
-                                    position: "absolute",
-                                    bottom: "0", 
-                                    right: "0", 
-                                    transform: "translate(25%, 25%)",
-                                }}>{cartQty}
-                            </aside>
+                        <img 
+                            width="30rem" height="30rem"
+                            src={cart2} alt="cart" 
+                        />
+                        <aside className="rounded-circle 
+                            bg-danger d-flex 
+                            justify-content-center 
+                            align-items-center"
+                            style={{
+                                color: "white", 
+                                width: "1.5rem", 
+                                height: "1.5rem",
+                                position: "absolute",
+                                bottom: "0", 
+                                right: "0", 
+                                transform: "translate(25%, 25%)",
+                            }}>{cartQty}
+                        </aside>
                     </Button>
-                </Nav>
+                )}
             </Container>
         </NavbarBS>
         </React.Fragment>
